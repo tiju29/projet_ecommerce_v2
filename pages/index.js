e8bee99b994547b7fa058763f1b86fe0d6e1c439
Julien Marier <julien.marier29@gmail.com>
2022-05-25 22:42:19 +0200

    Dynamic datas with Sanity OK

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,10 @@ const Home = ({ products, bannerData }) => (
 
       <div className="products-container">
         {products?.map(
-          (product) => product.name)}
+          (product) => <Product key={product._id} product={product} />)}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
 );
 
